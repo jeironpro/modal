89dfb25f03158a5cdd356cd4d146898fab4fb64e
jeironprogrammer <jeironprogrammer@gmail.com>
2025-09-30 02:38:13 +0200

    Creado como un repositorio individual

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirModal() {
+    document.getElementById('modal').style.display = 'block';
+    document.getElementById('modalOverlay').style.display = 'block';
+}
+
+function cerrarModal() {
+    document.getElementById('modal').style.display = 'none';
+    document.getElementById('modalOverlay').style.display = 'none';
+}
